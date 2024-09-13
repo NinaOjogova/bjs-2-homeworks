19bcdebacab8f726dab400410c3b77077b0e5dfc
Nina Ojogova <nnina190402@gmail.com>
2024-09-13 14:14:27 +0700

    Задание готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+  this.marks = [];
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
-
+new Student("Ольга", "Женский", 22);
+new Student("Николай", "Мужской", 34);
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+  if (this.marks) {
+    this.marks.push(...marksToAdd);
+  } else {
+    return 0;
+  }
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function (marks) {
+   if (this.marks && this.marks.length > 0) {
+    return this.marks.reduce((sum, current) => sum + current, 0) / this.marks.length;
+    } else {
+      return 0;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
